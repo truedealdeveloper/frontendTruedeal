be089925e9f66202a2a092438eb0b67536e76553
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2025-02-13 17:53:52 +0530

    added relative

--- src/app/group-tours/[destination]/layout.tsx
+++ src/app/group-tours/[destination]/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { groupToursData } from '@/data/groupTours';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export async function generateMetadata({ params }: { params: { destination: string } }): Promise<Metadata> {
   const tourData = groupToursData[params.destination];
