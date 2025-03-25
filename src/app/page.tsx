ce9b94396f85798b93bf5eba108cf9fd9c3215a2
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2025-03-25 11:44:26 +0530

    added more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <FixedDepartures />
       </section>
       <div className="mt-12">
-        {/* <ChardhamYatra /> */}
+        {/* <ChardhamYatra /> */} 
         <SriLanka />  
         <BaliBanner />
         <VietnamBanner />
