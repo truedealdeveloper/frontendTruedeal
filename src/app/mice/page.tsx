8374d97e434ab97ca7df0820887a76e26bf44b82
sourabh <sourabhbhatt825@gmail.com>
2025-04-16 18:55:58 +0530

    Added the page.tsx

--- src/app/mice/page.tsx
+++ src/app/mice/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { useRef, useState } from "react"
-import Image from "next/image"
+import { useRef} from "react"
 import Link from "next/link"
 import { motion } from "framer-motion"
 import {NumberTicker} from "@/components/magicui/number-ticker"
